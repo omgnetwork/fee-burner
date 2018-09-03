0fbcc08b9935811945c958401e9532fca15cb166
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-03 13:11:17 +0200

    Fix FeeBurner contract's tests

--- tests/test_fee_burner.py
+++ tests/test_fee_burner.py
@@ -128,22 +128,21 @@ def test_exchange_omg_for_some_token(non_operator, omg_token, fee_burner, other_
     assert other_token.functions.balanceOf(non_operator).call() == user_initial_balance + 1
 
 
-@pytest.mark.skip(reason="for some reason gas costs too much now")
 def test_exchange_omg_for_ether(w3, non_operator, omg_token, fee_burner):
     # given: a user adds adds allowance on OMG contract and user has some initial tokens
     fee_burner.transact().addSupportFor(ZERO_ADDRESS, 1, 1)
     omg_token.functions.approve(fee_burner.address, HUGE_AMOUNT).transact({'from': non_operator})
-    w3.eth.sendTransaction({'to': fee_burner.address, 'value': 10000000})
+    w3.eth.sendTransaction({'to': fee_burner.address, 'value': 10**18})
 
     user_initial_balance = w3.eth.getBalance(non_operator)
 
     # when: the user sends an exchange demand OMG for other token at rate 1,1 (initial rate)
-    fee_burner.functions.exchange(ZERO_ADDRESS, 1, 1, 10000000, 10000000).transact({'from': non_operator})
+    fee_burner.functions.exchange(ZERO_ADDRESS, 1, 1, 10**18, 10**18).transact({'from': non_operator})
 
     # then: user has received token and OMGs have been burnt
-    assert omg_token.functions.balanceOf(DEAD_ADDRESS).call() == 10000000
+    assert omg_token.functions.balanceOf(DEAD_ADDRESS).call() == 10**18
     assert w3.eth.getBalance(non_operator) > user_initial_balance
-    assert w3.eth.getBalance(non_operator) <= user_initial_balance + 10000000
+    assert w3.eth.getBalance(non_operator) <= user_initial_balance + 10**18
 
 
 def test_exchange_when_user_offers_more_omgs_then_needed(w3, non_operator, omg_token, fee_burner, other_token):
