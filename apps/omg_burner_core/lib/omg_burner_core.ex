5de2466f550bd90a63aad92c197b31eaef0e38f6
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-03 13:07:13 +0200

    Remove core module

--- apps/omg_burner_core/lib/omg_burner_core.ex
+++
@@ -1,3 +0,0 @@
-defmodule OMG.BurnerCore do
-
-end
--- apps/omg_burner_core/test/omg_burner_core_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OMG.BurnerCoreTest do
-  use ExUnit.Case
-  doctest OMG.BurnerCore
-
-  test "greets the world" do
-    assert OMG.BurnerCore.hello() == :world
-  end
-end
