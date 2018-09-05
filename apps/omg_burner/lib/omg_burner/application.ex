5d59f1a4cb59ff6f27344afb71221d5582cb7d5d
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-05 14:27:37 +0200

    Clean up

--- apps/omg_burner/lib/omg_burner/application.ex
+++ apps/omg_burner/lib/omg_burner/application.ex
@@ -6,14 +6,12 @@ defmodule OMG.Burner.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
+    # List all child  processes to be supervised
     children = [
       # Starts a worker by calling: OMG.Burner.Worker.start_link(arg)
       # {OMG.Burner.Worker, arg},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: OMG.Burner.Supervisor]
     Supervisor.start_link(children, opts)
   end
