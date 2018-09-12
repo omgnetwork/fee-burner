6871bc65ee599db8768a10398ee874487771dce5
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-12 16:34:39 +0200

    Add initial implementation of API

--- apps/omg_burner/lib/omg_burner.ex
+++ apps/omg_burner/lib/omg_burner.ex
@@ -1,17 +1,30 @@
 defmodule OMG.Burner do
   require Logger
 
+  alias OMG.Burner.State
+
   # API
   def add_fee_to_be_collected(token, value) do
-
+    State.add_fee(token, value)
   end
 
   def start_fee_exit(token) do
+    {:ok, value} = State.preexit_token(token)
+
+    Logger.info("Starting #{token} exit, have accumulated #{value} of tokens")
+
+    # TODO: send request to Contract module
 
   end
 
   def confirm_fee_exit_started(token) do
+    :ok = State.confirm_token_exited(token)
+    Logger.info("Confirmed token (#{token}})stared exit")
+  end
 
+  def cancel_token_exit(token) do
+    :ok = State.cancel_preexit(token)
+    Logger.info("Canceled token (#{token}}) exit")
   end
 
 end
