f6fc8835571c415ac23db9d3d09c8dc0b54b960a
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-12 15:28:41 +0200

    Change API

--- apps/omg_burner/lib/omg_burner.ex
+++ apps/omg_burner/lib/omg_burner.ex
@@ -1,8 +1,9 @@
 defmodule OMG.Burner do
-  use GenServer
+  require Logger
 
   # API
   def add_fee_to_be_collected(token, value) do
+
   end
 
   def start_fee_exit(token) do
@@ -12,17 +13,5 @@ defmodule OMG.Burner do
   def confirm_fee_exit_started(token) do
 
   end
-
-  # GenServer
-  def start_link()do
-    GenServer.start_link(__MODULE__, [])
-  end
-
-  def init([]) do
-
-  end
-
-  # handlers
-
 
 end
