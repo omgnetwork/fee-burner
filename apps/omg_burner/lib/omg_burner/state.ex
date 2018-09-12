a16016dfd78334edb9913d6b5779fba324bb9f5b
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-12 14:59:39 +0200

    Add OMG.Burner.State implementation

---
+++ apps/omg_burner/lib/omg_burner/state.ex
@@ -0,0 +1,141 @@
+defmodule OMG.Burner.State do
+
+  @type token_t() :: atom()
+  @type error_t() :: {:error, atom()}
+  @type status_t() :: :ok | error_t()
+  @type status_with_value_t :: {:ok, number()} | error_t()
+
+  @type accumulated_values_t() :: %{token_t() => number()}
+  @type state_t() :: {accumulated_values_t(), accumulated_values_t()}
+
+  require Logger
+
+  use GenServer
+
+  # API
+  def start_link(initial_state \\ {%{}, %{}}) do
+    GenServer.start_link(__MODULE__, initial_state, name: __MODULE__)
+  end
+
+  @spec add_fee(token_t(), number()) :: status_t()
+  def add_fee(token, value) do
+    GenServer.cast(__MODULE__, {:add_fee, token, value})
+    :ok
+  end
+
+  @spec preexit_token(token_t()) :: status_with_value_t()
+  def preexit_token(token) do
+    GenServer.call(__MODULE__, {:preexit, token})
+  end
+
+  @spec confirm_token_exited(token_t()) :: status_t()
+  def confirm_token_exited(token) do
+    GenServer.call(__MODULE__, {:confirm_exit, token})
+  end
+
+  @spec cancel_preexit(token_t()) :: status_t()
+  def cancel_preexit(token) do
+    GenServer.call(__MODULE__, {:cancel_exit, token})
+  end
+
+  @spec get_preexited_fees(token_t) :: number()
+  def get_preexited_fees(token) do
+    GenServer.call(__MODULE__, {:get_preexited, token})
+  end
+
+  @spec get_accumulated_fees(token_t()) :: number()
+  def get_accumulated_fees(token) do
+    GenServer.call(__MODULE__, {:get_accumulated, token})
+  end
+
+  # GenServer
+
+  def init({accumulated, preexited} = state) do
+    {:ok, state}
+  end
+
+  def handle_cast({:add_fee, token, value}, {accumulated, preexited} = _state) do
+
+    new_accumulated = do_add_fee(token, value, accumulated)
+    new_state = {new_accumulated, preexited}
+
+    {:noreply, new_state}
+
+  end
+
+  def handle_call({:preexit, token}, _from, state) do
+    {reply, new_state} = do_preexit(token, state)
+    {:reply, reply, new_state}
+  end
+
+  def handle_call({:get_accumulated, token}, _from, {accumulated, _} = state) do
+    {:reply, Map.get(accumulated, token, 0), state}
+  end
+
+  def handle_call({:get_preexited, token}, _from, {_, preexited} = state) do
+    {:reply, Map.get(preexited, token, 0), state}
+  end
+
+  def handle_call({:confirm_exit, token}, _from, {accumulated, preexited}) do
+    {reply, new_preexited} = do_confirm_exit(token, preexited)
+    {:reply, reply, {accumulated, new_preexited}}
+  end
+
+  def handle_call({:cancel_exit, token}, _from, state) do
+    {reply, new_state} = do_cancel_exit(token, state)
+    {:reply, reply, new_state}
+  end
+
+  # TODO: leave it ?
+  #  def handle_cast(_, _, state) do
+  #    {:noreply, state}
+  #  end
+  #
+  #  def handle_call(_, _from, state) do
+  #    {:reply, :error, state}
+  #  end
+
+  defp do_add_fee(token, value, accumulated) do
+    tmp = accumulated
+          |> Map.update(token, value, &(&1 + value))
+
+    # todo: move
+    if Map.get(tmp, token) > 0 do
+      tmp
+    else
+      Map.delete(tmp, token)
+    end
+  end
+
+  defp do_preexit(token, {accumulated, preexited} = state) do
+    with :error <- Map.fetch(preexited, token),
+         {:ok, value} <- Map.fetch(accumulated, token),
+         true <- value > 0 do
+      {{:ok, value}, move_to_preexited(token, state)}
+    else
+      {:ok, value} -> {{:error, :already_preexited}, state}
+      _ -> {{:error, :nothing_to_preexit}, state}
+    end
+  end
+
+  defp move_to_preexited(token, {accumulated, preexited}) do
+    {to_preexit, new_accumulated} = Map.pop(accumulated, token)
+    new_preexited = Map.put_new(preexited, token, to_preexit)
+    {new_accumulated, new_preexited}
+  end
+
+  defp do_confirm_exit(token, preexited) do
+    case Map.fetch(preexited, token) do
+      {:ok, _} -> {:ok, Map.delete(preexited, token)}
+      _ -> {{:error, :nothing_to_confirm}, preexited}
+    end
+  end
+
+  defp do_cancel_exit(token, {accumulated, preexited} = state) do
+    case Map.fetch(preexited, token) do
+      {:ok, value} -> {:ok, {do_add_fee(token, value, accumulated), Map.delete(preexited, token)}}
+      _ -> {{:error, :nothing_to_cancel}, state}
+    end
+  end
+
+end
---
+++ apps/omg_burner/test/state__test.exs
@@ -0,0 +1,155 @@
+defmodule OMG.Burner.StateTest do
+  use ExUnitFixtures
+  use ExUnit.Case
+
+  alias OMG.Burner.State
+
+  @test_value 10
+
+  setup do
+    {:ok, pid} = State.start_link()
+
+    State.add_fee(TOKEN0, @test_value)
+
+    {:ok, state: pid}
+  end
+
+  def test_token_values(token, accumulated, preexited) do
+    assert State.get_accumulated_fees(token) == accumulated
+    assert State.get_preexited_fees(token) == preexited
+    :ok
+  end
+
+  test "add initial fee" do
+
+    # initially state has accumulated no ETH
+    test_token_values(ETH, 0, 0)
+
+    # then add some ETH
+    :ok = State.add_fee(ETH, 1)
+    test_token_values(ETH, 1, 0)
+
+  end
+
+  test "update fee" do
+
+    :ok = State.add_fee(TOKEN0, 69)
+    test_token_values(TOKEN0, @test_value + 69, 0)
+
+  end
+
+  test "preexit fee" do
+
+    {:ok, @test_value} = State.preexit_token(TOKEN0)
+    test_token_values(TOKEN0, 0, @test_value)
+
+  end
+
+
+  test "try to preexit already preexited fee" do
+    # preexit token and add some tokens to accumulator
+    State.preexit_token(TOKEN0)
+    State.add_fee(TOKEN0, @test_value)
+
+
+    # preexitng the same token second time causes an error
+    {:error, :already_preexited} = State.preexit_token(TOKEN0)
+    test_token_values(TOKEN0, @test_value, @test_value)
+
+  end
+
+  test "confirm fee exited" do
+
+    State.preexit_token(TOKEN0)
+    :ok = State.confirm_token_exited(TOKEN0)
+
+    test_token_values(TOKEN0, 0, 0)
+
+  end
+
+
+  test "try preexit fee which accumulated value is 0" do
+
+    State.add_fee(ETH, 0)
+    {:error, :nothing_to_preexit} = State.preexit_token(ETH)
+
+  end
+
+
+  test "preexit and confirm twice" do
+
+    State.preexit_token(TOKEN0)
+    State.add_fee(TOKEN0, @test_value)
+    test_token_values(TOKEN0, @test_value, @test_value)
+
+    :ok = State.confirm_token_exited(TOKEN0)
+    test_token_values(TOKEN0, @test_value, 0)
+
+    State.preexit_token(TOKEN0)
+    :ok = State.confirm_token_exited(TOKEN0)
+    test_token_values(TOKEN0, 0, 0)
+
+  end
+
+  test "cancel token exit" do
+    State.preexit_token(TOKEN0)
+
+    :ok = State.cancel_preexit(TOKEN0)
+    test_token_values(TOKEN0, @test_value, 0)
+
+  end
+
+  test "preexit, add fees and cancel" do
+
+    State.preexit_token(TOKEN0)
+    State.add_fee(TOKEN0, @test_value)
+
+    :ok = State.cancel_preexit(TOKEN0)
+
+    test_token_values(TOKEN0, 2 * @test_value, 0)
+  end
+
+  test "try cancel when nothing to be canceled" do
+
+    {:error, :nothing_to_cancel} = State.cancel_preexit(TOKEN0)
+    test_token_values(TOKEN0, @test_value, 0)
+
+  end
+
+
+  test "adding, pre-exiting, canceling/confirming exitrs does not affect other tokens" do
+
+    State.add_fee(TOKEN1, @test_value)
+    State.add_fee(TOKEN2, @test_value)
+
+    State.preexit_token(TOKEN2)
+    # adding
+    State.add_fee(TOKEN0, 1)
+
+    test_token_values(TOKEN1, @test_value, 0)
+    test_token_values(TOKEN2, 0, @test_value)
+
+    # pre-exiting
+    State.preexit_token(TOKEN0)
+
+    test_token_values(TOKEN1, @test_value, 0)
+    test_token_values(TOKEN2, 0, @test_value)
+
+    # confirmation
+
+    State.confirm_token_exited(TOKEN0)
+
+    test_token_values(TOKEN1, @test_value, 0)
+    test_token_values(TOKEN2, 0, @test_value)
+
+    # canceling
+    State.add_fee(TOKEN0, 1)
+    State.preexit_token(TOKEN0)
+    State.cancel_preexit(TOKEN0)
+
+    test_token_values(TOKEN1, @test_value, 0)
+    test_token_values(TOKEN2, 0, @test_value)
+
+  end
+
+end
