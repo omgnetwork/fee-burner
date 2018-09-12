37d76de8e5b3cb4cc9ef7f0c74efc366b7b02ce7
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-09-12 18:31:01 +0200

    Write initial state of threshold agent tests

--- apps/omg_burner/test/threshold_agent_test.exs
+++ apps/omg_burner/test/threshold_agent_test.exs
@@ -2,4 +2,11 @@ defmodule OMG.Burner.ThresholdAgentTest do
   use ExUnitFixtures
   use ExUnit.Case
 
+  alias OMG.Burner.ThresholdAgent, as: Agent
+
+  setup() do
+    Agent.start_link()
+    :ok
+  end
+
 end
